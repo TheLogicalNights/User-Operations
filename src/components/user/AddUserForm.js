432f0c857529dd10737761c85152031bc99109d0
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-21 19:41:40 +0530

    Rendered error modal

--- src/components/user/AddUserForm.js
+++ src/components/user/AddUserForm.js
@@ -3,6 +3,7 @@ import Card from "../ui/Card";
 import Button from "../ui/Button";
 import "../ui/Button.css";
 import { useState } from "react";
+import ErrorModal from "../ui/ErrorModal";
 
 const AddUserForm = (props) => {
   const [enteredUserName, setUserName] = useState("");
@@ -35,38 +36,41 @@ const AddUserForm = (props) => {
     props.onAddUser(enteredUserName, enteredAge);
   };
   return (
-    <Card className={`${"input"}`}>
-      <form onSubmit={onSubmitHandler}>
-        <label
-          htmlFor="username"
-          className={`${isValidUsername ? "" : "invalid"}`}
-        >
-          Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          onChange={usernameChangedHandler}
-          value={enteredUserName}
-        />
-        <label
-          htmlFor="age (Years)"
-          className={`${isValidAge ? "" : "invalid"}`}
-        >
-          Age
-        </label>
-        <input
-          type="number"
-          id="age"
-          className={`${isValidAge ? "" : "invalid"}`}
-          onChange={ageChangedHandler}
-          value={enteredAge}
-        />
-        <Button className="button" type="submit">
-          Add User
-        </Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="Something happens" message="Something went wrong !" />
+      <Card className={`${"input"}`}>
+        <form onSubmit={onSubmitHandler}>
+          <label
+            htmlFor="username"
+            className={`${isValidUsername ? "" : "invalid"}`}
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            onChange={usernameChangedHandler}
+            value={enteredUserName}
+          />
+          <label
+            htmlFor="age (Years)"
+            className={`${isValidAge ? "" : "invalid"}`}
+          >
+            Age
+          </label>
+          <input
+            type="number"
+            id="age"
+            className={`${isValidAge ? "" : "invalid"}`}
+            onChange={ageChangedHandler}
+            value={enteredAge}
+          />
+          <Button className="button" type="submit">
+            Add User
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
