9f646ad7ab8a7c866601401f26c8718a480c0e02
Swapnil Adhav <swapniladhav560@gmail.com>
2021-09-21 17:19:44 +0530

    Added state of users list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import AddUserForm from "./components/user/AddUserForm";
 import UsersList from "./components/user/UsersList";
+import { useState } from "react";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, age) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { id: Math.round(Math.random() * 1000000), name: username, age: age },
+      ];
+    });
+  };
   return (
     <div>
-      <AddUserForm />
-      <UsersList users={[]} />
+      <AddUserForm onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
